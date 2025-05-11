cd22512102691166dd2fd6954f6d525e99efa6db
Suhani <115005183+XSuhaniSinghX@users.noreply.github.com>
2025-05-11 15:25:05 +0530

    Create AppTest.java

---
+++ src/test/java/your/pack/AppTest.java
@@ -0,0 +1,13 @@
+package your.pack;  // Match your package
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        // Just run it and test for output
+        Main.main(new String[]{});
+    }
+}
